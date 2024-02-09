9fc940b518de2581d46c9339d3e69113f6f6da37
Sajal <sajalkumarr030@gmail.com>
2024-02-09 22:53:54 +0530

    initial

---
+++ server.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+const moment = require("moment");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const botName = "ChitChat App";
+
+const chatMessages = [];
+
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    socket.emit("message", formatMessage(botName, "Welcome to ChitChat App"));
+
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+
+    socket.emit("chatHistory", chatMessages);
+  });
+
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    const message = {
+      username: user.username,
+      text: msg,
+      time: moment().format("h:mm A"),
+    };
+    chatMessages.push(message);
+
+    io.to(user.room).emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
